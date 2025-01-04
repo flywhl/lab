ebf73847ec94cc5f0e690748b5952224964ed04f
Rory Byrne <rory@rory.bio>
2025-01-04 14:08:34 +0000

    feat: Add plan command to generate execution plan from Labfile

--- lab/cli/commands/plan.py
+++ lab/cli/commands/plan.py
@@ -1,19 +1,20 @@
 from pathlib import Path
+from typing import Annotated
 from dishka import FromDishka
 import rich
-import click
+import typer
 
 from lab.core.ui import UserInterface
 from lab.project.service.labfile import LabfileService
 from lab.project.service.plan import PlanService
 
 
-@click.command()
-@click.argument('path', type=click.Path(exists=True, path_type=Path))
-def plan(path: Path, _ui: FromDishka[UserInterface]):
-    """Generate execution plan from Labfile"""
+def plan(
+    path: Annotated[Path, typer.Argument(help="Path to Labfile")],
+    ui: FromDishka[UserInterface],
+):
     rich.print(f"Generating plan for [b]{path.resolve()}[/b]\n")
-    print(_ui)
+    print(ui)
     labfile_service = LabfileService()
     plan_service = PlanService()
     project = labfile_service.parse(path)
